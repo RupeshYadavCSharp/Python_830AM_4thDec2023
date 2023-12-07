bd7444a1d248dc499b291762b35f98cf05bbd031
SwastikCr7g <swastikgahukarcr7@gmail.com>
2023-12-07 23:01:04 +0530

    homework - 3

---
+++ Swastik_Code/demo/p_hello/homework 5.py
@@ -0,0 +1,19 @@
+print ("BILL DETAILS")
+a = 100
+b = 20
+c = 10
+print ("price of pizza is",a)
+print ("price of puffs is",b)
+print ("price of cold drinks is",c)
+print ("no of pizza : ")
+x = float(input())
+print ("no of puffs : ")
+y = float(input())
+print ("no of cold drinks : ")
+z = float(input())
+print ("total cost of pizza is",a*x)
+print ("total cost of puffs is",b*y)
+print ("total cost of cold drinks is",c*z)
+print ("total price of above expenditure is",(a*x)+(b*y)+(c*z))
+print ("THANK YOU ")
+print ("ENJOY THE SHOW !!!")
