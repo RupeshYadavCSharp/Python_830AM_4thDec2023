32171be135c468225499fa3b9ab4c924994ecb21
SwastikCr7g <swastikgahukarcr7@gmail.com>
2023-12-07 16:50:32 +0530

    homework - 1

--- Swastik_Code/demo/p_hello/homework 2.py
+++
@@ -1,4 +0,0 @@
-print ("enter a")
-a = float(input())
-b = (a*9/5)+32
-print ("celsius into fahrenheit is ",b)
---
+++ Swastik_Code/demo/p_hello/homework 3.py
@@ -0,0 +1,13 @@
+a = 67
+b = 89
+c = 100
+d = 100
+print ("each paper was of",d,"marks ")
+print ("marks earned in maths is",a)
+print ("marks earned in phy is",b)
+print ("marks earned in chem is",c)
+print ("total marks earned is",a+b+c)
+print ("total number of marks is",d*3)
+e = 256
+print ("total earned marks is",e)
+print ("total percentage is",e/300*100)
