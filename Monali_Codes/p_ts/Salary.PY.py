588926382bc4fa5380407fe7223b05186059a541
monaliisah <healthylife257@gmail.com>
2023-12-08 09:29:24 +0530

    8dec

---
+++ Monali_Codes/p_ts/Salary.PY.py
@@ -0,0 +1,9 @@
+print("enter basic salary:-")
+sal=float(input())
+da=35/100*sal
+ha=40/100*sal
+total_sal=da+ha+sal
+print("Basic salary is",sal)
+print("DA is",da)
+print("HA is",ha)
+print("Total salary is",total_sal)
