58d5d0fff11477d065080524a21afdf20d644b93
RupeshYadavCSharp <81669954+RupeshYadavCSharp@users.noreply.github.com>
2023-12-08 09:02:25 +0530

    changes added to swastik code

---
+++ Swastik_Code/demo/p_hello/Percent.py
@@ -0,0 +1,15 @@
+print("Enter marks in math")
+math = int(input())
+
+print("Enter marks in phy ")
+phy = int(input())
+
+print("Enter marks in chem ")
+chem = int(input())
+
+total = math+phy+chem
+
+per = total/3
+
+print("total marks are ",total)
+print("percent obtained are ",per )
