8ebc000a01b2cb5f3da7f49486f6742ceccffe8e
SwastikCr7g <swastikgahukarcr7@gmail.com>
2023-12-11 11:19:34 +0530

    bank

--- Swastik_Code/demo/p_hello/p_reverse the no/Bank.py
+++ Swastik_Code/demo/p_hello/p_reverse the no/Bank.py
@@ -3,5 +3,5 @@ num = int(input("enter amount\n",))
 print ("500 *",num//500)
 a = num - (num//500*500)
 print ("200 *",(a//200))
-b = ((num - ((num//500)*500))//200*200)
-print ("100 *",(b//100))
+b = a // 200
+print ("100 *",(num - ((500*(num//500))+(200*(b))))//100)
