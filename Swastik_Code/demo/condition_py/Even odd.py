485d457b4183062aadc84c9075038eb5b08ef253
SwastikCr7g <swastikgahukarcr7@gmail.com>
2023-12-15 15:54:00 +0530

    odd and even

--- Swastik_Code/demo/condition_py/Even odd.py
+++ Swastik_Code/demo/condition_py/Even odd.py
@@ -1,5 +1,6 @@
-num = int(input("enter the no \n"))
-if(num%2==0):
-    print(num,"is even no")
+#enter any number
+num = int(input("enter the no \n"))               #num=10
+if(num%2==0):                                     #10%2==0=0
+    print(num,"is even no")                       #10 is even no
 else:
     print(num,"is odd no")
