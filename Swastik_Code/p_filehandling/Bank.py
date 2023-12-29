65d5de5f9292ba65e86992f83822ae16c370f230
SwastikCr7g <swastikgahukarcr7@gmail.com>
2023-12-29 23:41:47 +0530

    TODO solved

--- Swastik_Code/p_filehandling/Bank.py
+++ Swastik_Code/p_filehandling/Bank.py
@@ -1,20 +1,39 @@
-ff=open("D:\\pin.txt","w")
-pin=input("enter pin")
-print(pin)
-ff.write(pin)
-ff.close()
+upin=int(input("enter your pin"))
+f=open("D:\\pin.txt","r")
+fpin=int(f.read())
+f.close()
+if(upin==fpin):
+    f=open("D:\\balance.txt","r")
+    bal=int(f.read())
+    f.close()
+    print("current balance is",bal)
+    choice=int(input("1.withdraw 2.deposit 3.change pin"))
+    if(choice==1):
+        amt=int(input("enter amount to be withdraw"))
+        bal=bal-amt
+        f=open("D:\\balance.txt","w")
+        f.write(str(bal))
+        f.close()
+    elif(choice==2):
+        amt = int(input("enter amount to be deposit"))
+        bal = bal + amt
+        f = open("D:\\balance.txt", "w")
+        f.write(str(bal))
+        f.close()
+    elif(choice==3):
+        ppin=int(input("enter current pin"))
+        f=open("D:\\pin.txt","r")
+        fpin=int(f.read())
+        f.close()
+        if(ppin==fpin):
+            npin = int(input("enter new pin"))
+            f=open("D:\\pin.txt","w")
+            f.write(str(npin))
+            f.close()
+        else:
+            print("your current pin is invalid")
 
-sf=open("D:\\balance.txt","a")
-bal=input("enter bal")
-print(bal)
-sf.write(bal)
-sf.close()
+        npin=int(input("enter new pin"))
 
-print("enter your pin")
-if(pin==1234):
-    print("your balace is ",bal)
-    a=withdraw
-    b=deposit
-    print("choose one")
-elif(pin!=1234):
-    print("your pin is invalid")
+else:
+    print("Pin is invalid")
